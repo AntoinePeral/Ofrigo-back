d7a5bea0fe0ecb5e1e37215e941ffe14d7953590
AntoinePeral <peral.antoine@gmail.com>
2023-04-17 16:34:31 +0200

    Add_function convertQuantity fixed

--- app/api/migration/deploy/4.add_functions.sql
+++ app/api/migration/deploy/4.add_functions.sql
@@ -1,5 +1,5 @@
 
-
+-- Fonction Pour récupérer toutes les recettes
 CREATE OR REPLACE FUNCTION getAllRecipes () 
   RETURNS TABLE (
     id int,
@@ -64,6 +64,7 @@ ORDER BY r.id ASC
 $$ LANGUAGE SQL;
 
 
+--  Fonction création de convert des quantity
 CREATE OR REPLACE FUNCTION convertQuantity(r_id int, i_id int) RETURNS TABLE (ingredient_quantity text)  AS $$
 SELECT
 	CASE
@@ -85,55 +86,7 @@ SELECT
 		WHEN i.unit='rouleau' THEN CONCAT(riq.ingredient_quantity, ' ','rouleau')
 		WHEN i.unit='coeur' THEN CONCAT(riq.ingredient_quantity, ' ','coeur')
 		WHEN i.unit='c.à.c' THEN CONCAT(riq.ingredient_quantity, ' ','c.à.c')
-	END AS ingredient_quantitySELECT
-r.id,
-r.label AS label,
-r.picture,
-r.rate,
-r.difficulty,
-r.time,
-r.created_at,
-r.updated_at,
-(
-	SELECT
-		json_agg(
-			json_build_object(
-				'id', i.id,
-				'label', i.label,
-				'quantity', riq.ingredient_quantity,
-				'unit', i.unit,
-				'ingredient_quantity', (SELECT * FROM convertQuantity(r.id, i.id)),
-				'created_at', i.created_at,
-				'updated_at', i.updated_at,
-				'category_id', c.id,
-				'category_label', c.label,
-				'category_created_at', c.created_at,
-				'category_updated_at', c.updated_at
-			)
-			ORDER BY i.created_at
-		)
-	FROM ingredient i
-	JOIN recipe_has_ingredient_with_quantity riq ON ingredient_id = i.id
-	JOIN category c ON c.id = i.category_id
-	WHERE recipe_id = r.id
-) AS ingredient,
-(
-	SELECT
-		json_agg(s.* ORDER BY s.created_at)
-	FROM step s
-	WHERE s.recipe_id = r.id
-) AS step,
-(
-	SELECT
-		json_agg(t.* ORDER BY t.label ASC)
-	FROM tag t
-	JOIN recipe_has_tag rt ON tag_id = t.id
-	WHERE recipe_id = r.id
-) AS tag
-FROM recipe r
-JOIN recipe_has_ingredient_with_quantity riq ON riq.recipe_id = r.id
-GROUP BY r.id
-ORDER BY r.id ASC
+	END AS ingredient_quantity
 FROM ingredient i
 JOIN recipe_has_ingredient_with_quantity riq
 ON riq.ingredient_id = i.id
