04700c82a5d433aaf3b542e06c29faa2304d97a2
AntoinePeral <peral.antoine@gmail.com>
2023-04-18 09:37:56 +0200

    pull fixed convert_function

--- app/api/migration/deploy/4.add_functions.sql
+++ app/api/migration/deploy/4.add_functions.sql
@@ -37,54 +37,52 @@ CREATE OR REPLACE FUNCTION getAllRecipes ()
 	steps json,
     tag json ) AS $$
 SELECT
-r.id,
-r.label AS label,
-r.picture,
-r.rate,
-r.difficulty,
-r.time,
-r.created_at,
-r.updated_at,
-(
-	SELECT 
-		json_agg(
-			json_build_object(
-				'id', i.id,
-				'label', i.label,
-				'quantity', riq.ingredient_quantity,
-				'ingredient_quantity', (SELECT * FROM convertQuantity(r.id, i.id)),
-				'unit', i.unit,
-				'created_at', i.created_at,
-				'updated_at', i.updated_at,
-				'category_id', c.id,
-				'category_label', c.label,
-				'category_created_at', c.created_at,
-				'category_updated_at', c.updated_at
-			) 
-			ORDER BY i.created_at
-		)
-	FROM ingredient i 
-	JOIN recipe_has_ingredient_with_quantity riq ON ingredient_id = i.id
-	JOIN category c ON c.id = i.category_id
-	WHERE recipe_id = r.id
-) AS ingredient,
-(
-	SELECT 
-		json_agg(s.* ORDER BY s.created_at) 
-	FROM step s 
-	WHERE s.recipe_id = r.id
-) AS step,
-(
-	SELECT 
-		json_agg(t.* ORDER BY t.label ASC) 
-	FROM tag t 
-	JOIN recipe_has_tag rt ON tag_id = t.id
-	WHERE recipe_id = r.id
-) AS tag
-FROM recipe r
-JOIN recipe_has_ingredient_with_quantity riq ON riq.recipe_id = r.id
-GROUP BY r.id
-ORDER BY r.id ASC
+	r.id,
+	r.label AS label,
+	r.picture,
+	r.rate,
+	r.difficulty,
+	r.time,
+	r.created_at,
+	r.updated_at,
+	(
+			SELECT 
+					json_agg(
+							json_build_object(
+									'id', i.id,
+									'label', i.label,
+									'quantity', (SELECT * FROM convertQuantity(r.id, i.id)),
+									'created_at', i.created_at,
+									'updated_at', i.updated_at,
+									'category_id', c.id,
+									'category_label', c.label,
+									'category_created_at', c.created_at,
+									'category_updated_at', c.updated_at
+							) 
+							ORDER BY i.created_at
+					)
+			FROM ingredient i 
+			JOIN recipe_has_ingredient_with_quantity riq ON ingredient_id = i.id
+			JOIN category c ON c.id = i.category_id
+			WHERE recipe_id = r.id
+	) AS ingredient,
+	(
+			SELECT 
+					json_agg(s.* ORDER BY s.created_at) 
+			FROM step s 
+			WHERE s.recipe_id = r.id
+	) AS step,
+	(
+			SELECT 
+					json_agg(t.* ORDER BY t.label ASC) 
+			FROM tag t 
+			JOIN recipe_has_tag rt ON tag_id = t.id
+			WHERE recipe_id = r.id
+	) AS tag
+	FROM recipe r
+	JOIN recipe_has_ingredient_with_quantity riq ON riq.recipe_id = r.id
+	GROUP BY r.id
+	ORDER BY r.id ASC;
 $$ LANGUAGE SQL;
 
 
@@ -105,53 +103,51 @@ CREATE OR REPLACE FUNCTION getOneRecipe (r_id int)
 	steps json,
     tag json ) AS $$
 SELECT
-r.id,
-r.label AS label,
-r.picture,
-r.rate,
-r.difficulty,
-r.time,
-r.created_at,
-r.updated_at,
-(
-	SELECT 
-		json_agg(
-			json_build_object(
-				'id', i.id,
-				'label', i.label,
-				'quantity', riq.ingredient_quantity,
-				'ingredient_quantity', (SELECT * FROM convertQuantity(r.id, i.id)),
-				'unit', i.unit,
-				'created_at', i.created_at,
-				'updated_at', i.updated_at,
-				'category_id', c.id,
-				'category_label', c.label,
-				'category_created_at', c.created_at,
-				'category_updated_at', c.updated_at
-			) 
-			ORDER BY i.created_at
-		)
-	FROM ingredient i 
-	JOIN recipe_has_ingredient_with_quantity riq ON ingredient_id = i.id
-	JOIN category c ON c.id = i.category_id
-	WHERE recipe_id = r.id
-) AS ingredient,
-(
-	SELECT 
-		json_agg(s.* ORDER BY s.created_at) 
-	FROM step s 
-	WHERE s.recipe_id = r.id
-) AS step,
-(
-	SELECT 
-		json_agg(t.* ORDER BY t.label ASC) 
-	FROM tag t 
-	JOIN recipe_has_tag rt ON tag_id = t.id
-	WHERE recipe_id = r.id
-) AS tag
-FROM recipe r
-JOIN recipe_has_ingredient_with_quantity riq ON riq.recipe_id = r.id
-WHERE r.id = r_id
-GROUP BY r.id
-ORDER BY r.id ASC
+	r.id,
+	r.label AS label,
+	r.picture,
+	r.rate,
+	r.difficulty,
+	r.time,
+	r.created_at,
+	r.updated_at,
+	(
+			SELECT 
+					json_agg(
+							json_build_object(
+									'id', i.id,
+									'label', i.label,
+									'quantity', (SELECT * FROM convertQuantity(r.id, i.id)),
+									'created_at', i.created_at,
+									'updated_at', i.updated_at,
+									'category_id', c.id,
+									'category_label', c.label,
+									'category_created_at', c.created_at,
+									'category_updated_at', c.updated_at
+							) 
+							ORDER BY i.created_at
+					)
+			FROM ingredient i 
+			JOIN recipe_has_ingredient_with_quantity riq ON ingredient_id = i.id
+			JOIN category c ON c.id = i.category_id
+			WHERE recipe_id = r.id
+	) AS ingredient,
+	(
+			SELECT 
+					json_agg(s.* ORDER BY s.created_at) 
+			FROM step s 
+			WHERE s.recipe_id = r.id
+	) AS step,
+	(
+			SELECT 
+					json_agg(t.* ORDER BY t.label ASC) 
+			FROM tag t 
+			JOIN recipe_has_tag rt ON tag_id = t.id
+			WHERE recipe_id = r.id
+	) AS tag
+	FROM recipe r
+	JOIN recipe_has_ingredient_with_quantity riq ON riq.recipe_id = r.id
+	WHERE r.id=r_id
+	GROUP BY r.id
+	ORDER BY r.id ASC;
 $$ LANGUAGE SQL;
