70014b18b9ae7295700f0a1eb29dd6ae42f55c90
Kevin Lienard <k.lienard.o.clock@gmail.com>
2023-04-21 16:09:26 +0200

    CRUD-v3

--- app/api/router/account.js
+++ app/api/router/account.js
@@ -9,7 +9,7 @@ accountRouter.get("/account", account.getAllAccount);
 accountRouter.get("/account/:id(\\d+)", account.getAccountById);
 accountRouter.get("/me/profile",middleware.authenticateToken, account.getUserAccount);
 accountRouter.post("/profile", account.addAccount);
-//accountRouter.put("/profile/:id(\\d+)", middleware.authenticateToken,validationModule.validateUserAccount('body'), account.updateAccount);
+accountRouter.put("/profile/:id(\\d+)", middleware.authenticateToken,validationModule.validateUserAccount('body'), account.updateAccount);
 accountRouter.delete("/profile/:id(\\d+)", middleware.authenticateToken, account.deleteAccount);
 
 // User
