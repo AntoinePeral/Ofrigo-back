0be09fae56f4a1aa12172906f628791ff3d34841
Kevin Lienard <k.lienard.o.clock@gmail.com>
2023-04-21 10:17:13 +0200

    fix server

--- app/api/router/account.js
+++ app/api/router/account.js
@@ -14,8 +14,8 @@ const middleware = require ('../../service/middleware/authToken');
 
 // User
 accountRouter.post("/register", validationModule.validateUserAccount('body'), account.addAccount);
-accountRouter.get("/me/profile",authentificationModule.authenticateToken, account.getUserAccount);
-accountRouter.put("/profile/:id(\\d+)", authentificationModule.authenticateToken,validationModule.validateUserAccount('body'), account.updateAccount);
-accountRouter.delete("/profile/:id(\\d+)", authentificationModule.authenticateToken, account.deleteAccount);
+accountRouter.get("/me/profile", middleware.authenticateToken, account.getUserAccount);
+accountRouter.put("/profile/:id(\\d+)", middleware.authenticateToken, validationModule.validateUserAccount('body'), account.updateAccount);
+accountRouter.delete("/profile/:id(\\d+)", middleware.authenticateToken, account.deleteAccount);
 
 module.exports = accountRouter;
