f7d6bdb62accdcaa33c641e1eed7b521505dce15
AntoinePeral <peral.antoine@gmail.com>
2023-04-21 16:02:18 +0200

    fix

--- app/api/router/account.js
+++ app/api/router/account.js
@@ -7,9 +7,9 @@ const middleware = require ('../../service/middleware/authToken');
 // Admin
 accountRouter.get("/account", account.getAllAccount);
 accountRouter.get("/account/:id(\\d+)", account.getAccountById);
-accountRouter.get("/me/profile",middleware.authenticateToken, account.getUserAccount);
-accountRouter.put("/profile/:id(\\d+)", middleware.authenticateToken,validationModule.validateUserAccount('body'), account.updateAccount);
-accountRouter.delete("/profile/:id(\\d+)", middleware.authenticateToken, account.deleteAccount);
+// accountRouter.get("/me/profile",middleware.authenticateToken, account.getUserAccount);
+// accountRouter.put("/profile/:id(\\d+)", middleware.authenticateToken,validationModule.validateUserAccount('body'), account.updateAccount);
+// accountRouter.delete("/profile/:id(\\d+)", middleware.authenticateToken, account.deleteAccount);
 
 // User
 accountRouter.post("/register", validationModule.validateUserAccount('body'), account.addAccount);
