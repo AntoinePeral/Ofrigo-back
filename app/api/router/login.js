80402fa47f35d8155950e2ed4873460d35db771b
Kevin Lienard <k.lienard.o.clock@gmail.com>
2023-04-25 12:03:57 +0200

    fixe contraint

--- app/api/router/login.js
+++ app/api/router/login.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const authentificationRouter = Router();
 const { login } = require("../controller");
+const validationRole = require("../../service/middleware/adminValidator");
 
 authentificationRouter.post("/login", login.signIn);
-authentificationRouter.post("/admin/login", login.signIn);
+authentificationRouter.post("/admin/login", validationRole.isAdmin, login.signIn);
 
 module.exports = authentificationRouter;
