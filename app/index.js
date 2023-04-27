f5f9b2ae3304d55011cd1ce7113720e12489a936
AntoinePeral <peral.antoine@gmail.com>
2023-04-27 13:05:28 +0200

    add ejs in index

--- app/index.js
+++ app/index.js
@@ -4,6 +4,10 @@ const app = express();
 const errorModule = require("./service/error/errorHandling");
 const { account, login, category, ingredient, recipe, message, tag } = require("./api/router");
 
+app.set('view engine', 'ejs');
+app.set('views', 'app/back/views');
+app.use(express.static("./app/back/asset"));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
