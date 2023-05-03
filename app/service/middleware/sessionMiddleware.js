6dde0bd35c9944038a4e16a812d80242fdbea313
Kevin Lienard <k.lienard.o.clock@gmail.com>
2023-05-03 21:44:48 +0200

    final version Back-office

--- app/service/middleware/sessionMiddleware.js
+++ app/service/middleware/sessionMiddleware.js
@@ -18,9 +18,6 @@ async function addUserToLocals(req, res, next) {
 
     req.session.user = user;
     res.locals.userLogin = user;
-
-    console.log("locals",res.locals.userLogin);
-    // console.log("locals2 avec req",req.locals.userLogin.email);
     
     next();
 }
