c5d70daf4575ef3fc5c308d64a68db92dab7fce0
Kevin Lienard <k.lienard.o.clock@gmail.com>
2023-05-04 16:27:02 +0200

    maj insomnia + JSDOC

--- app/back/router/tag.js
+++ app/back/router/tag.js
@@ -8,9 +8,9 @@ const { isAdmin } = require("../../service/middleware");
 tagRouter.get("/admin/tag", isAdmin, home.menu, tag.getAllTagPage);
 tagRouter.get("/admin/tag/:id(\\d+)", isAdmin, home.menu, tag.getTagPage);
 tagRouter.get("/admin/tag/create", isAdmin, home.menu, tag.getCreateTagPage);
-tagRouter.post("/admin/tag/create", isAdmin, home.menu, /*validationBody.validateAdminTag('body'),*/ tag.addTag);
+tagRouter.post("/admin/tag/create", isAdmin, home.menu, validationBody.validateAdminTag('body'), tag.addTag);
 tagRouter.get("/admin/tag/update/:id(\\d+)", isAdmin, home.menu, tag.getCreateTagPage);
-tagRouter.post("/admin/tag/update/:id(\\d+)", isAdmin, home.menu, /*validationBody.validateAdminTag('body'),*/ tag.updateTag);
+tagRouter.post("/admin/tag/update/:id(\\d+)", isAdmin, home.menu, validationBody.validateAdminTag('body'), tag.updateTag);
 tagRouter.post("/admin/tag/delete/:id(\\d+)", isAdmin, home.menu, tag.deleteTag);
 tagRouter.post("/admin/tag/:tagId(\\d+)/recipe/:recipeId(\\d+)", isAdmin, home.menu, tag.removeRecipeFromTag);
 
